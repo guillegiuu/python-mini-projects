6fccfe3a1dc9ae0a8a17147d61a5e9b178173977
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-18 00:02:16 -0300

    Update-remove-name-details-01-block_letters.py

--- mini-projects/01-block_letters.py
+++ mini-projects/01-block_letters.py
@@ -2,14 +2,7 @@
 # Proyecto: Block Letters
 # Curso: Codecademy Python
 # Descripción: Generador de letras en ASCII
-# Parte 2 
 # ================================
-
-
-# Guillermo Giusso
-
-
-# Me gusta estudiar programación con apuntes claros y prolijos
 
 print("Mis iniciales en ASCII art:\n")
 
